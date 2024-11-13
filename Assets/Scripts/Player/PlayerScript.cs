92544c1ff2f8f60a9c64e63590aea1b456e74a54
Mixer2048 <mixerchannel4@gmail.com>
2024-11-13 16:14:39 +0700

    secondUpdate

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public WeaponSelector
+    public WeaponSelector weaponSelector;
+
+    public int numberOfWeapons = 4;
 
     void Update()
     {
-        
+        float mouseWheelDelta = Input.GetAxisRaw("Mouse ScrollWheel");
+        if (mouseWheelDelta > 0) weaponSelector.selectNextWeapon();
+        if (mouseWheelDelta < 0) weaponSelector.selectPrevWeapon();
+    }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+
+        if (e.type == EventType.KeyDown)
+        {
+            int k = (int)(e.keyCode - KeyCode.Alpha0);
+            if (!(k > 0 && k <= numberOfWeapons))
+            {
+                k = (int)(e.keyCode - KeyCode.Keypad0);
+
+                if (!(k > 0 && k <= numberOfWeapons))
+                    return;
+            }
+            weaponSelector.selectWeaponByIndex(k - 2);
+        }
     }
 }
