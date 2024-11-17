136fb2091e25b424328d01c7ea52894b9dac294d
Mixer2048 <mixerchannel4@gmail.com>
2024-11-17 20:00:29 +0700

    3WeaponsAdded

---
+++ Assets/Scripts/Weapons/CWeapon.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CWeapon : MonoBehaviour, IWeapon
+{
+    public Transform firePoint;
+    public float fireRate;
+    public float damage;
+    [HideInInspector]
+    public bool canFire = true;
+    public ParticleSystem weaponEffect;
+
+    private void Awake()
+    {
+        if (weaponEffect == null) return;
+
+        var main = weaponEffect.main;
+        main.duration = 1 / fireRate;
+    }
+
+    public virtual void fire()
+    {
+        canFire = false;
+        StartCoroutine(coolDown());
+    }
+
+    IEnumerator coolDown()
+    {
+        yield return new WaitForSeconds(1 / fireRate);
+        canFire = true;
+    }
+
+    private void OnEnable()
+    {
+        if (canFire == false)
+            StartCoroutine(coolDown());
+    }
+}
