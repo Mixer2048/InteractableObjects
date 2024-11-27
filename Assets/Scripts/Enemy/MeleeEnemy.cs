651603ede623c9b563af6392e1f703789e36a20a
Launaft <147025777+Launaft@users.noreply.github.com>
2024-11-27 14:49:41 +0700

    MeleeEnemy

---
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class MeleeEnemy : AbstractEnemy
+{
+    [Range(0.1f, 10)] public float AttackRange = 2f;
+    [Range(1, 10)] public int Damage = 4;
+
+    RunTo _runState;
+    RotateTo _rotateState;
+    Attack _attackState;
+    Stunned _stunnedState;
+
+    private new void Start()
+    {
+        base.Start();
+
+        _runState = new RunTo(this);
+        _rotateState = new RotateTo(this);
+        _attackState = new Attack(this);
+        _stunnedState = new Stunned(this);
+
+        stateMachine.StartState(_runState);
+    }
+
+    public void UpdateState()
+    {
+        if (dead) return;
+
+        if (stunned) stateMachine?.SetState(_stunnedState);
+        else
+        {
+            if (Vector3.Angle(transform.forward, player.position - transform.position) > 20f)
+                stateMachine?.SetState(_rotateState);
+            else if (Vector3.Distance(transform.position, player.position) > AttackRange)
+                stateMachine?.SetState(_runState);
+            else if (Vector3.Angle(transform.forward, player.position - transform.position) > 10f)
+                stateMachine?.SetState(_rotateState);
+            else
+                stateMachine?.SetState(_attackState);
+        }
+
+        stateMachine?.Update();
+    }
+
+    public void DealDamage()
+    {
+        if (Vector3.Distance(transform.position, player.position) <= AttackRange)
+        {
+            Health playerHP = player.GetComponent<Health>();
+
+            if (playerHP != null)
+                playerHP.hpDecrease(Damage);
+        }
+    }
+}
