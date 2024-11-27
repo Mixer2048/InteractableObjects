4ffa5a0e48d355100669a7d2874b0d55804f1e23
Launaft <147025777+Launaft@users.noreply.github.com>
2024-11-27 14:24:26 +0700

    AbstractEnemy script

--- Assets/Scripts/Player/Data/Health.cs
+++ Assets/Scripts/Player/Data/Health.cs
@@ -8,6 +8,12 @@ public class Health : MonoBehaviour
     [SerializeField, Range(1, 100)] private int maxHealth;
     [SerializeField, Range(1, 100)] private int currentHealth;
 
+    public UnityEvent<Vector3> spawnOnDeath;
+    public UnityEvent onDeath;
+    public UnityEvent onHitTaken;
+
+    private void Start() => OnHealthChanged?.Invoke(currentHealth, maxHealth);
+
     public bool ChangeHealth(int amount)
     {
         if (currentHealth == maxHealth) return false;
@@ -26,4 +32,24 @@ public class Health : MonoBehaviour
     }
 
     private void Awake() => OnHealthChanged?.Invoke(currentHealth, maxHealth);
+
+    public void hpDecrease(float amount)
+    {
+        if (currentHealth <= 0) return;
+
+        onHitTaken?.Invoke();
+
+        currentHealth = Mathf.FloorToInt(currentHealth - amount);
+
+        if (currentHealth < 0)
+            currentHealth = 0;
+
+        OnHealthChanged?.Invoke(currentHealth, maxHealth);
+
+        if (currentHealth <= 0)
+        {
+            onDeath?.Invoke();
+            spawnOnDeath?.Invoke(transform.position);
+        }
+    }
 }
