e3687b9dbdc46dd05e563b5d6dd199edf7928ec6
Launaft <147025777+Launaft@users.noreply.github.com>
2024-11-27 13:10:15 +0700

    finished

--- Assets/Scripts/Items/ItemSpawner.cs
+++ Assets/Scripts/Items/ItemSpawner.cs
@@ -3,18 +3,36 @@ using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
-    [SerializeField] List<ItemFactory> itemFactories = new List<ItemFactory>();
-    
+    [SerializeField] private List<ItemProbability> itemProbabilities = new List<ItemProbability>();
+    private List<ItemFactory> itemFactories = new List<ItemFactory>();
+
+    ItemFactory _itemFactory;
+
     public void SpawnRandomItem()
     {
-        ItemFactory itemFactory = itemFactories[Random.Range(0, itemFactories.Count)];
-        IItem item = itemFactory.GetItem();
+        _itemFactory = itemFactories[Random.Range(0, itemFactories.Count)];
+        IItem item = _itemFactory.GetItem();
 
         Vector3 direction = new Vector3(Random.insideUnitCircle.x, 0, Random.insideUnitCircle.y);
         direction = direction.normalized * Random.Range(3, 6);
         Vector3 position = transform.position + direction;
 
         item.SetPosition(position);
+    }
+
+    private void Start()
+    {
+        float probabilitySum = 0f;
+
+        foreach (var item in itemProbabilities)
+            probabilitySum += item.probability;
+
+        foreach (var item in itemProbabilities)
+            item.probability = Mathf.Floor((item.probability / probabilitySum) * 100);
+
+        foreach (var item in itemProbabilities)
+            for (int i = 0; i < item.probability; i++)
+                itemFactories.Add(item.factory);
     }
 
     private void Update()
